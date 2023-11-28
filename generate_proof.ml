5d10dc81826b2d605c8e867e2eb05a6469f30da5
ZachhRobin <150301092+ZachhRobin@users.noreply.github.com>
2023-11-27 18:41:12 -0600

    Group Call

---
+++ generate_proof.ml
@@ -0,0 +1,29 @@
+(* Our rewriter for code that is parsed *)
+include Ast
+include Declaration
+
+module Parser = parser 
+module Lexer = lexer
+
+let parse_expression (s:string) : expression = 
+  let lexbuf = Lexing.from_string s in 
+  let ast = Parser.expression_eof
+    Lexer.token lexbuf in ast
+
+let get_lhs eq =
+    Equality(e1 : expression, _) -> e1 
+let get_rhs eq =
+    Equality(e1 : equality, e2 : expression) -> e2
+
+(* Matches expression with something it should be equal to  *)
+let rec match_expression vars eq expr =
+  match get_lhs eq with
+
+let rec attempt_rewrites (vars: string list) (eq: Equality) (expr: Expression) : (Expression option) =
+    
+
+and rec attempt_rewrite (eq: equality) = 
+    match eq with
+    | Equality(e1: expression, e2 :expression) -> Some e1
+    | LLinkedEq(e1, _) -> attempt_rewrite e1
+    | _ -> None
